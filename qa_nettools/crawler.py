31395edd73840fa0f4964a36e94b5ea4324f2599
Sam Gleske <sam.mxracer@gmail.com>
2014-02-19 12:24:12 -0800

    crawler delay supported

--- qa_nettools/crawler.py
+++ qa_nettools/crawler.py
@@ -7,6 +7,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from sys import stderr
+from time import sleep
 
 class crawler():
   """
@@ -24,11 +25,12 @@ class crawler():
     Careful, this script is designed to crawl a whole website and find all pages.  If a site is large enough consider splitting up the work and refining the domain_filter.
   """
   pages={}
-  def __init__(self,driver,domain_filter="example.com"):
+  def __init__(self,driver,domain_filter="example.com",delay=0):
     if domain_filter == "example.com":
       print >> stderr, "Warning: your crawler is initialized with example.com."
     self.driver=driver
     self.domain_filter=domain_filter
+    self.delay=delay
   def _consume(self,url):
     try:
       if '?' in url:
@@ -38,6 +40,8 @@ class crawler():
       self.pages[url]=[]
       self.driver.get(url)
       tags=[]
+      if self.delay > 0:
+        sleep(self.delay)
       if len(self.driver.find_elements_by_tag_name('a')) > 0:
         for tag in self.driver.find_elements_by_tag_name('a'):
           if not type(tag.get_attribute('href')) == type(None):
