bc5a98cbf817674b72a88fac75d134de5f9cf2e2
Sam Gleske <sam.mxracer@gmail.com>
2014-02-26 14:47:30 -0800

    Page class finished

--- qa_nettools/triage.py
+++ qa_nettools/triage.py
@@ -5,21 +5,73 @@
 #Linux 3.11.0-12-generic x86_64
 #Python 2.7.5+
 
+class Page():
+  resource_count=0
+  resources=[]
+  link_count=0
+  highpriority=False
+  mediumpriority=False
+  links=[]
+  def __init__(self):
+    pass
+  def add_resource(self,ref,status):
+    #triage the resource
+    if not self.highpriority and status == "404":
+      self.highpriority=True
+    if not self.mediumpriority and not status == "401":
+      self.mediumpriority=True
+    #increment the resource count
+    self.resource_count+=1
+    if (self.resource_count+self.link_count) >= 5:
+      self.highpriority=True
+    #append the resource and status
+    self.resources.append((ref,status))
+
+  def add_link(self,ref,status):
+    #triage the link
+    if not self.highpriority and status == "404":
+      self.highpriority=True
+    if not self.mediumpriority and not status == "401":
+      self.mediumpriority=True
+    #increment the resource count
+    self.link_count+=1
+    if (self.resource_count+self.link_count) >= 5:
+      self.highpriority=True
+    #append the resource and status
+    self.links.append((ref,status))
+    pass
+  def add_screenshot(self,data):
+    """
+    Not implemented.
+    """
+    pass
+
 class triage():
   """
   Triage report generator attempts to make sense of the QA results for the layman web developer.
   """
-  def __init__(self,total_tests=0,failed_tests=0,runtime="0s"):
-    self.total_tests=total_tests
-    self.failed_tests=failed_tests
-    self.runtime=runtime
+  #priorities
+  _high=[]
+  _medium=[]
+  _low=[]
+  _pages={}
+  def __init__(self):
+    pass
+  def set_summary(self,total_tests=0,failed_tests=0,runtime="0s"):
+    self._total_tests=total_tests
+    self._failed_tests=failed_tests
+    self._runtime=runtime
   def report(self):
+    #Report Summary
     report=["# Summary",""]
     report+=["Unit tests",
              "",
-             "* Passed: `%d`" % (self.total_tests-self.failed_tests),
-             "* Failed: `%d`" % self.failed_tests,
-             "* Total: `%d`" % self.total_tests,
+             "* Passed: `%d`" % (self._total_tests-self._failed_tests),
+             "* Failed: `%d`" % self._failed_tests,
+             "* Total: `%d`" % self._total_tests,
+             "* Run time: `%s`" % self._runtime,
              "",
-             "Run time: `%s`" % self.runtime]
+             "Issues are triaged into three categories: High, Medium, Low.  Each issue is in the form of a link to the page that contains the issue followed by a bullet point list of issues discovered on that page.  There may be an analysis section at the end of this document which might be worth checking out before viewing prioritized issues.  For a description of the HTTP status codes contained in this document please see [rfc2616.10](http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html).",
+             ""]
+    #Prioritized items
     return '\n'.join(report)
