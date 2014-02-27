927b7d88a2c88a20bd8ce3f2a365a70c74c2532b
Sam Gleske <sam.mxracer@gmail.com>
2014-02-27 16:16:22 -0500

    adding pages implemented with categorizing statuses

--- qa_nettools/triage.py
+++ qa_nettools/triage.py
@@ -5,6 +5,8 @@
 #Linux 3.11.0-12-generic x86_64
 #Python 2.7.5+
 
+import re
+
 class Page():
   resource_count=0
   resources=[]
@@ -55,12 +57,35 @@ class triage():
   _medium=[]
   _low=[]
   _pages={}
+  _found404=False
+  _found401=False
+  _found30x=False
   def __init__(self):
     pass
   def set_summary(self,total_tests=0,failed_tests=0,runtime="0s"):
     self._total_tests=total_tests
     self._failed_tests=failed_tests
     self._runtime=runtime
+  def add_link(self,page,ref,status):
+    if not self._found404 and status == "404":
+      self._found404=True
+    elif not self._found401 and status == "401":
+      self._found401=True
+    elif not self._found30x and if re.match(r'^30[0-7]$',status):
+      self._found30x=True
+    if not page in self._pages.keys():
+      self._pages[page]=Page()
+    self._pages[page].add_link(ref=ref,status=status)
+  def add_resource(self,page,ref,status):
+    if not self._found404 and status == "404":
+      self._found404=True
+    elif not self._found401 and status == "401":
+      self._found401=True
+    elif not self._found30x and if re.match(r'^30[0-7]$',status):
+      self._found30x=True
+    if not page in self._pages.keys():
+      self._pages[page]=Page()
+    self._pages[page].add_resource(ref=ref,status=status)
   def report(self):
     #Report Summary
     report=["# Summary",""]
