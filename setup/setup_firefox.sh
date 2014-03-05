9c67902e587e2d33c7e7c762687cfcba87ef6634
Sam Gleske <sam.mxracer@gmail.com>
2014-03-05 11:21:10 -0800

    update setup script

--- setup/setup_firefox.sh
+++ setup/setup_firefox.sh
@@ -11,7 +11,7 @@
 set -e
 
 if [ "$1" = "kill" ];then
-  killall -9 Xvfb java firefox
+  killall -9 Xvfb java firefox openbox
   exit
 fi
 
@@ -26,7 +26,7 @@ elif [ "$USER" = "root" ];then
   apt-get update
 
   # Install java, firefox, pip, unzip, and Xvfb
-  apt-get -y install openjdk-7-jre unzip python-pip firefox xvfb xfonts-cyrillic xfonts-100dpi xfonts-75dpi xfonts-tipa
+  apt-get -y install openjdk-7-jre unzip python-pip firefox xvfb xfonts-cyrillic xfonts-100dpi xfonts-75dpi xfonts-tipa openbox
   #install selenium bindings for python
   pip install selenium
   #markdown2html prereqs
@@ -60,15 +60,22 @@ export DISPLAY=:10
 
 echo "Starting Xvfb ..."
 if ! pgrep Xvfb &> /dev/null;then
-  Xvfb :10 +extension RANDR -screen 0 1366x768x24 -ac &
+#  Xvfb :10 +extension RANDR -screen 0 1366x768x24 -ac &> /dev/null &
+  Xvfb :10 +extension RANDR -screen 0 1920x1080x24 -ac &> /dev/null &
 else
   echo "Xvfb already running."
 fi
+#echo "Starting openbox window manager..."
+#if ! pgrep openbox &> /dev/null;then
+#  openbox &> /dev/null &
+#else
+#  echo "openbox already running."
+#fi
 firefox &>/dev/null &
 echo "Starting Selenium ..."
 cd /usr/local/bin
 if ! pgrep java &> /dev/null;then
-  nohup java -jar ./selenium-server-standalone-2.39.0.jar &
+  nohup java -jar ./selenium-server-standalone-2.39.0.jar &> /dev/null &
 else 
   echo "java already running."
 fi
